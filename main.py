67543525f993f4732e13837b327b37bc79e0d178
Shervin the Prodigy <68794773+Shervi28@users.noreply.github.com>
2021-08-06 22:40:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-########################################################################################
-######################          Import packages      ###################################
-########################################################################################
+
 from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 from __init__ import create_app, db
 
-########################################################################################
-# our main blueprint
+
+
 main = Blueprint('main', __name__)
 
-@main.route('/') # home page that return 'index'
+@main.route('/')
 def index():
     return render_template('index.html')
 
-@main.route('/profile') # profile page that return 'profile'
+@main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
 
-app = create_app() # we initialize our flask app using the __init__.py function
+app = create_app()
 if __name__ == '__main__':
-    db.create_all(app=create_app()) # create the SQLite database
-    app.run(debug=True) # run the flask app on debug mode
+    db.create_all(app=create_app())
+    app.run(debug=True) 
